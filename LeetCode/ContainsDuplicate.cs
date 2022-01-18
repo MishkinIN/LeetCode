909b88de8ffbaf61b1671a9926b29804eec0d037
Ivan <Mishkin_Ivan@mail.ru>
2022-01-18 17:45:13 +0400

    18-01-2022

---
+++ LeetCode/ContainsDuplicate.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode {
+    public static partial class Solution {
+        /*
+         * Given an integer array nums, 
+         * return true if any value appears at least twice in the array, 
+         * and return false if every element is distinct.  
+         * 
+    1 <= nums.length <= 10^5
+    -10^9 <= nums[i] <= 10^9
+
+         */
+        public static bool ContainsDuplicate_WidthHashSet(int[] nums) {
+            HashSet<int> hs = new();
+            foreach (var item in nums) {
+                if (!hs.Add(item)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool ContainsDuplicate(int[] nums) {
+            return !NotFindDuplicateAndSort(nums, 0, (uint)nums.Length);
+
+
+        }
+        private static bool NotFindDuplicateAndSort(int[] nums, uint start, uint end) {
+            var lenght = end - start;
+            int acc;
+            if (lenght < 2)
+                return true;
+            if (lenght == 2) {
+                if (nums[start] == nums[end - 1]) {
+                    return false;
+                }
+                else {
+                    if (nums[start] > nums[end - 1]) {
+                        acc = nums[start];
+                        nums[start] = nums[end - 1];
+                        nums[end - 1] = acc;
+                    }
+                    return true;
+                }
+            }
+            var center = (end + start) / 2;
+            if (NotFindDuplicateAndSort(nums, start, center) && NotFindDuplicateAndSort(nums, center, end)) {
+                uint leftindex = start, rightindex = center;
+                Queue<int> queue = new();
+                while (leftindex < center) {
+                    queue.Enqueue(nums[leftindex]);
+                    acc = queue.Peek() - nums[rightindex];
+                    if (acc == 0) {
+                        return false;
+                    }
+                    else {
+                        if (acc < 0) {
+                            nums[leftindex++] = queue.Dequeue();
+                        }
+                        else {
+                            nums[leftindex++] = nums[rightindex++];
+                        }
+                    }
+                }
+                while (queue.Count > 0 & rightindex < end) {
+                    acc = queue.Peek() - nums[rightindex];
+                    if (acc == 0) {
+                        return false;
+                    }
+                    else {
+                        if (acc < 0) {
+                            nums[leftindex++] = queue.Dequeue();
+                        }
+                        else {
+                            nums[leftindex] = nums[rightindex];
+                            leftindex++;
+                            rightindex++;
+                        }
+                    }
+                }
+                while (queue.Count > 0) {
+                    nums[leftindex++] = queue.Dequeue();
+                }
+
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
--- LeetCode/Solution.cs
+++ LeetCode/Solution.cs
@@ -4,45 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCode
-{
-    public static partial class Solution
-    {
-        public static int[] TwoSum(int[] nums, int target)
-        {
+namespace LeetCode {
+    public static partial class Solution {
+        public static int[] TwoSum(int[] nums, int target) {
             int length = nums.Length;
             int[] keys = new int[length];
-            for (int k = 0; k < length; k++)
-            {
+            for (int k = 0; k < length; k++) {
                 keys[k] = k;
             }
             Array.Sort(nums, keys);
             int i, j = 0;
-            for (i = 0; i < length - 1; i++)
-            {
+            for (i = 0; i < length - 1; i++) {
                 j = Array.BinarySearch(nums, i + 1, length - i - 1, target - nums[i]);
-                if (j > 0)
-                {
+                if (j > 0) {
                     return new int[] { keys[i], keys[j] };
                 }
             }
             throw new ArgumentOutOfRangeException();
         }
-        public static bool IsPalindrome(int x)
-        {
+        public static bool IsPalindrome(int x) {
             if (x < 0)
                 return false;
             else if (x == 0)
                 return true;
             int[] digits = new int[10];
             int i = 0, j = -1;
-            do
-            {
+            do {
                 digits[++j] = x % 10;
                 x /= 10;
             } while (x > 0);
-            do
-            {
+            do {
                 if (digits[i] != digits[j])
                     return false;
                 if (i + 1 == j && (digits[i] == digits[j]))
@@ -52,14 +43,11 @@ namespace LeetCode
             } while (i < j);
             return i == j;
         }
-        public static int RomanToInt(string s)
-        {
+        public static int RomanToInt(string s) {
             int sub = 1;
             int sum = 0;
-            for (int k = s.Length - 1; k >= 0; k--)
-            {
-                switch (s[k])
-                {
+            for (int k = s.Length - 1; k >= 0; k--) {
+                switch (s[k]) {
                     case 'I':
                         sub = sub > 1 ? -1 : 1;
                         break;
@@ -88,22 +76,18 @@ namespace LeetCode
             }
             return sum;
         }
-        public static string LongestCommonPrefix(string[] strs)
-        {
+        public static string LongestCommonPrefix(string[] strs) {
             string prefix = strs[0];
             int prefixLength = prefix.Length;
             string current;
-            for (int k = 1; k < strs.Length; k++)
-            {
+            for (int k = 1; k < strs.Length; k++) {
                 current = strs[k];
-                if (prefixLength > current.Length)
-                {
+                if (prefixLength > current.Length) {
                     prefixLength = current.Length;
                     prefix = prefix[0..current.Length];
                 }
-                
-                while (current[0..prefixLength]!=prefix)
-                {
+
+                while (current[0..prefixLength] != prefix) {
                     prefixLength--;
                     prefix = prefix[0..prefixLength];
                 }
@@ -112,26 +96,21 @@ namespace LeetCode
             }
             return prefix;
         }
-        public static string LongestCommonPrefix_1(string[] strs)
-        {
-            if (strs.Length==1)
-            {
+        public static string LongestCommonPrefix_1(string[] strs) {
+            if (strs.Length == 1) {
                 return strs[0];
             }
-            var enums = strs.Select(str=>str.GetEnumerator()).ToArray();
+            var enums = strs.Select(str => str.GetEnumerator()).ToArray();
             int prefLength = 0;
             Char current = '_';
-            while (enums.All(en => en.MoveNext()) && enums.All(en =>
-              {
-                  if (current == '_')
-                  {
-                      current = en.Current;
-                      return true;
-                  }
-                  else
-                      return current == en.Current;
-              }))
-            {
+            while (enums.All(en => en.MoveNext()) && enums.All(en => {
+                if (current == '_') {
+                    current = en.Current;
+                    return true;
+                }
+                else
+                    return current == en.Current;
+            })) {
                 current = '_';
                 prefLength++;
             }
@@ -144,14 +123,11 @@ namespace LeetCode
         /// </summary>
         /// <param name="s">s.Length>1</param>
         /// <returns></returns>
-        public static bool IsValid(string s)
-        {
+        public static bool IsValid(string s) {
             byte a = 1, b = 2, c = 4;
             var stack = new Stack<byte>(10000);
-            foreach (var ch in s)
-            {
-                switch (ch)
-                {
+            foreach (var ch in s) {
+                switch (ch) {
                     case '(':
                         stack.Push(a);
                         break;
@@ -162,20 +138,17 @@ namespace LeetCode
                         stack.Push(c);
                         break;
                     case ')':
-                        if (stack.Count == 0 || stack.Pop() != a)
-                        {
+                        if (stack.Count == 0 || stack.Pop() != a) {
                             return false;
                         }
                         break;
                     case '}':
-                        if (stack.Count == 0 || stack.Pop() != b)
-                        {
+                        if (stack.Count == 0 || stack.Pop() != b) {
                             return false;
                         }
                         break;
                     case ']':
-                        if (stack.Count == 0 || stack.Pop() != c)
-                        {
+                        if (stack.Count == 0 || stack.Pop() != c) {
                             return false;
                         }
                         break;
@@ -185,5 +158,37 @@ namespace LeetCode
             }
             return stack.Count == 0;
         }
+
+        public static bool CanPlaceFlowers(int[] fl, int n) {
+            int i = 0;
+            if (n == 0)
+                return true;
+            int flLength = fl.Length;
+            if (2 * n > flLength + 1)
+                return false;
+            bool isRightAdjanced = false;
+            while (i < flLength - 1) {
+                isRightAdjanced = fl[i + 1] != 0;
+                if (isRightAdjanced) {
+                    i += 3;
+                    //isRightAdjanced=false;
+                }
+                else if (fl[i] == 0) {
+                    if (--n == 0)
+                        return true;
+                    i += 2;
+                }
+                else
+                    i += 2;
+            }
+            if (i < flLength)
+                return fl[i] == 0 && --n == 0;
+            else
+                return false;
+        }
+        public static int Search(int[] nums, int target) {
+            return Array.BinarySearch(nums, target) < 0 ? -1 : Array.BinarySearch(nums, target);
+        }
+ 
     }
 }

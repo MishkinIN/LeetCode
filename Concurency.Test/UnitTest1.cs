117b42f04bf12b6ecbe5fad5df29c8f81dbcdee8
Mishkin I.N <Mishkin_Ivan@mail.ru>
2022-02-06 22:41:15 +0400

    06-02-2022

--- Concurency.Test/UnitTest1.cs
+++ Concurency.Test/UnitTest1.cs
@@ -1,10 +1,55 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
 
 namespace Concurency.Test {
     [TestClass]
-    public class UnitTest1 {
+    public class UnitTestThreading {
         [TestMethod]
         public void TestMethod1() {
+            string s = "Создание нового потока из тела подпрограммы WriteY";
+            Thread t = new Thread(() => Log(s));
+            // Запуск потока:
+            t.Start();
+            // Вместе с работающим потоком внутри WriteY() также будем
+            // выполнять что-нибудь в главном потоке приложения:
+            for (int i = 0; i < 1000; i++)
+                Console.Write("x");
         }
+        public static void Log(string s) {
+            Console.WriteLine(s);
+        }
+        [TestMethod]
+        public void TestMethod2() {
+            new Thread(Go).Start();   // Вызов Go() в новом потоке.
+            Go();                      // Вызов Go() в основном потоке приложения.
+        }
+        static void Go() {
+            // Декларирование и использование локальной переменной cycles:
+            for (int cycles = 0; cycles < 1000; cycles++)
+                Console.Write($"|{cycles}|");
+        }
+        [TestMethod]
+        public void TestMethod3() {
+               // Создание общего экземпляра
+            for (int i = 0; i < 5; i++) {
+                ThreadTest tt = new();
+                new Thread(tt.Go).Start(); 
+            }
+            //tt.Go();
+        }
+
+    }
+    public class ThreadTest {
+        static bool done;
+        // Обратите внимание, что Go теперь метод экземпляра:
+        internal void Go() {
+            if (!done) {
+               Console.WriteLine("Done");
+                 done = true;
+            }
+        }
+
     }
 }
